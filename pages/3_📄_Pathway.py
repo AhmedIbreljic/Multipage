ee140d2fd85abc86ded5dc944ca1ae4d8159354c
Ahmed Ibreljic <ahmedibreljic@gmail.com>
2023-08-27 13:05:21 -0400

    Update 3_📄_Pathway.py

--- pages/3_📄_Pathway.py
+++ pages/3_📄_Pathway.py
@@ -29,46 +29,28 @@ def electrical_engineering_degree():
 
     st.write("### Electrical Engineering Undergraduate Degree Courses")
 
-    course_data = []
-    for year, courses in degree_data.items():
-        for course in courses:
-            course_data.append({"Year": year, "Course": course["Course"], "Credits": course["Credits"], "Type": course["Type"]})
-    df = pd.DataFrame(course_data)
-
-    pivot_df = df.pivot(index="Course", columns="Year", values="Credits").reset_index()
-    pivot_df.columns.name = None
-
-    # Create a styled DataFrame with a color bar for course classification
-    styled_df = pivot_df.copy()
-    for year in pivot_df.columns[1:]:
-        styled_df[year] = pivot_df[year].apply(lambda x: f'<div style="background-color: #ff9896" class="{x}">{x}</div>' if x == "Math" else
-                                                          f'<div style="background-color: #aec7e8" class="{x}">{x}</div>' if x == "EE" else
-                                                          f'<div style="background-color: #98df8a" class="{x}">{x}</div>' if x == "Science" else
-                                                          f'<div style="background-color: #ffbb78" class="{x}">{x}</div>')
-    
-    styled_df = styled_df.style.render(escape=False)
-
-    # Display the styled DataFrame with the color-coded bars
-    st.write(styled_df, unsafe_allow_html=True)
+    df = pd.DataFrame([(year, course["Course"], course["Credits"], course["Type"]) for year, courses in degree_data.items() for course in courses],
+                      columns=["Year", "Course", "Credits", "Type"])
+    st.dataframe(df)
 
     chart = alt.Chart(df).mark_bar().encode(
-        x=alt.X("sum(Credits):Q", title="Total Credits"),
-        y=alt.Y("Course:N", title="Course"),
+        x=alt.X("Year:N", title="Year"),
+        y=alt.Y("sum(Credits):Q", title="Total Credits"),
         color=alt.Color("Type:N", title="Course Classification",
                         scale=alt.Scale(scheme="category20")),
-        tooltip=["Course", "sum(Credits)", "Type"]
+        tooltip=["Year", "sum(Credits)", "Type"]
     ).properties(
-        title="Total Credits per Course"
+        title="Total Credits per Year"
     )
 
-    st.write("### Total Credits per Course")
+    st.write("### Total Credits per Year")
     st.altair_chart(chart, use_container_width=True)
 
 st.set_page_config(page_title="Electrical Engineering Degree", page_icon="⚡")
 st.markdown("# Electrical Engineering Degree")
 st.sidebar.header("Electrical Engineering Degree")
 st.write(
-    """This demo showcases an example of an Electrical Engineering undergraduate degree with sample courses over four years. Courses are color-coded based on their classification. Each course name is highlighted with a vertical color bar indicating its classification."""
+    """This demo showcases an example of an Electrical Engineering undergraduate degree with sample courses over four years. Courses are color-coded based on their classification."""
 )
 
 electrical_engineering_degree()
