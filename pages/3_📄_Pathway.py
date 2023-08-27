640ded120776c693514d8efbb0438f72bf0951c4
Ahmed Ibreljic <ahmedibreljic@gmail.com>
2023-08-27 12:57:05 -0400

    Update 3_📄_Pathway.py

--- pages/3_📄_Pathway.py
+++ pages/3_📄_Pathway.py
@@ -1,37 +1,56 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import random
 
 def electrical_engineering_degree():
     degree_data = {
-        "Year 1": ["Mathematics", "Introduction to Electrical Engineering", "Physics"],
-        "Year 2": ["Circuit Analysis", "Digital Logic Design", "Signals and Systems"],
-        "Year 3": ["Electromagnetics", "Control Systems", "Electronics"],
-        "Year 4": ["Power Systems", "Communication Systems", "Senior Design Project"],
+        "Year 1": [
+            {"Course": "Mathematics", "Credits": random.randint(3, 5), "Type": "Math"},
+            {"Course": "Introduction to EE", "Credits": random.randint(3, 5), "Type": "EE"},
+            {"Course": "Physics", "Credits": random.randint(3, 5), "Type": "Science"},
+        ],
+        "Year 2": [
+            {"Course": "Circuit Analysis", "Credits": random.randint(3, 5), "Type": "EE"},
+            {"Course": "Digital Logic Design", "Credits": random.randint(3, 5), "Type": "EE"},
+            {"Course": "Signals and Systems", "Credits": random.randint(3, 5), "Type": "EE"},
+        ],
+        "Year 3": [
+            {"Course": "Electromagnetics", "Credits": random.randint(3, 5), "Type": "EE"},
+            {"Course": "Control Systems", "Credits": random.randint(3, 5), "Type": "EE"},
+            {"Course": "Electronics", "Credits": random.randint(3, 5), "Type": "EE"},
+        ],
+        "Year 4": [
+            {"Course": "Power Systems", "Credits": random.randint(3, 5), "Type": "EE"},
+            {"Course": "Communication Systems", "Credits": random.randint(3, 5), "Type": "EE"},
+            {"Course": "Senior Design Project", "Credits": random.randint(3, 5), "Type": "Project"},
+        ],
     }
 
     st.write("### Electrical Engineering Undergraduate Degree Courses")
 
-    degree_df = pd.DataFrame(degree_data)
-    st.dataframe(degree_df)
+    df = pd.DataFrame([(year, course["Course"], course["Credits"], course["Type"]) for year, courses in degree_data.items() for course in courses],
+                      columns=["Year", "Course", "Credits", "Type"])
+    st.dataframe(df)
 
-    melted_df = pd.melt(degree_df, var_name="Year", value_name="Course")
-    chart = alt.Chart(melted_df).mark_bar().encode(
-        x="Year:N",
-        y="count()",
-        color="Year:N",
-        column="Year:N",
-        tooltip="Course",
+    chart = alt.Chart(df).mark_bar().encode(
+        x=alt.X("Year:N", title="Year"),
+        y=alt.Y("sum(Credits):Q", title="Total Credits"),
+        color=alt.Color("Type:N", title="Course Classification",
+                        scale=alt.Scale(scheme="category20")),
+        tooltip=["Year", "sum(Credits)", "Type"]
+    ).properties(
+        title="Total Credits per Year"
     )
 
-    st.write("### Course Distribution Over Four Years")
+    st.write("### Total Credits per Year")
     st.altair_chart(chart, use_container_width=True)
 
 st.set_page_config(page_title="Electrical Engineering Degree", page_icon="⚡")
 st.markdown("# Electrical Engineering Degree")
 st.sidebar.header("Electrical Engineering Degree")
 st.write(
-    """This demo showcases an example of an Electrical Engineering undergraduate degree with sample courses over four years."""
+    """This demo showcases an example of an Electrical Engineering undergraduate degree with sample courses over four years. Courses are color-coded based on their classification."""
 )
 
 electrical_engineering_degree()
