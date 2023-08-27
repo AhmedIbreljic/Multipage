08f251f90c33f51d81a55b93d00bacc7775925c0
Ahmed Ibreljic <ahmedibreljic@gmail.com>
2023-08-27 13:15:30 -0400

    Update 📊_TreeSearch.py

--- pages/📊_TreeSearch.py
+++ pages/📊_TreeSearch.py
@@ -1,56 +1,96 @@
+# Copyright 2018-2022 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import streamlit as st
-import itertools
-import plotly.express as px
+import inspect
+import textwrap
+import time
+import numpy as np
+from utils import show_code
 
-def treesearch_demo():
-    st.write("### Electrical Engineering Degree Tree Search")
-    
-    # Dummy course data (replace this with your actual course data)
-    course_data = [
-        {"Course": f"Course {i}", "Credits": 3 + i % 3} for i in range(45)
-    ]
-    
-    total_credits_required = 126
-    max_years = 4
-    
-    # Generate all possible combinations of courses
-    combinations = []
-    for r in range(1, len(course_data) + 1):
-        combinations.extend(itertools.combinations(course_data, r))
-    
-    # Filter valid combinations based on total credits and years
-    valid_combinations = []
-    for combo in combinations:
-        total_credits = sum(course["Credits"] for course in combo)
-        years = len(combo)
-        if total_credits >= total_credits_required and years <= max_years:
-            valid_combinations.append(combo)
-    
-    # Create a Plotly figure
-    fig = px.scatter(
-        valid_combinations,
-        x=[len(combo) for combo in valid_combinations],
-        y=[sum(course["Credits"] for course in combo) for combo in valid_combinations],
-        labels={"x": "Number of Courses", "y": "Total Credits"},
-        title="Possible Combinations of EE Courses"
-    )
-    
-    # Add constraint lines
-    fig.add_hline(y=total_credits_required, line_dash="dash", line_color="green", name=f"{total_credits_required} Credits")
-    fig.add_vline(x=max_years * 4, line_dash="dash", line_color="red", name=f"{max_years} Years")
-    
-    st.plotly_chart(fig)
 
-    st.write(
-        """This plot shows all possible combinations of Electrical Engineering courses for students to graduate in 4 years or less. Each point represents a combination with the number of courses on the x-axis and the total credits on the y-axis. The red dashed line represents the 4-year constraint, and the green dashed line represents the 126-credit constraint."""
-    )
+def plotting_demo():
+    progress_bar = st.sidebar.progress(0)
+    status_text = st.sidebar.empty()
+    last_rows = np.random.randn(1, 1)
+    chart = st.line_chart(last_rows)
 
-# Streamlit app layout
-st.set_page_config(page_title="Tree Search Demo", page_icon="🌳")
-st.markdown("# Tree Search Demo")
-st.sidebar.header("Tree Search Demo")
+    for i in range(1, 101):
+        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+        status_text.text("%i%% Complete" % i)
+        chart.add_rows(new_rows)
+        progress_bar.progress(i)
+        last_rows = new_rows
+        time.sleep(0.05)
+
+    progress_bar.empty()
+
+    # Streamlit widgets automatically run the script from top to bottom. Since
+    # this button is not connected to any other logic, it just causes a plain
+    # rerun.
+    st.button("Re-run")
+
+
+st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+st.markdown("# Plotting Demo")
+st.sidebar.header("Plotting Demo")
 st.write(
-    """This demo generates all possible combinations of Electrical Engineering courses for students to graduate in 4 years or less. The total number of courses is 45, and the required credit amount is 126."""
+    """This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!"""
 )
 
-treesearch_demo()
+plotting_demo()
+
+show_code(plotting_demo)
+ChatGPT
+The code you provided is a Streamlit app that demonstrates plotting and animation using the streamlit library. It generates random data and displays it in a line chart while updating the chart in a loop to create an animation. Here's your code with explanations for each part:
+
+python
+Copy code
+import streamlit as st
+import time
+import numpy as np
+from utils import show_code
+
+def plotting_demo():
+    progress_bar = st.sidebar.progress(0)
+    status_text = st.sidebar.empty()
+    last_rows = np.random.randn(1, 1)
+    chart = st.line_chart(last_rows)
+
+    for i in range(1, 101):
+        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+        status_text.text("%i%% Complete" % i)
+        chart.add_rows(new_rows)
+        progress_bar.progress(i)
+        last_rows = new_rows
+        time.sleep(0.05)
+
+    progress_bar.empty()
+
+    st.button("Re-run")
+
+st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+st.markdown("# Plotting Demo")
+st.sidebar.header("Plotting Demo")
+st.write(
+    """This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!"""
+)
+
+plotting_demo()
+
+show_code(plotting_demo)
